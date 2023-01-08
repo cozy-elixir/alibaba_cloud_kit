053b2a5e76a3bc271ba00b989937428d2a6914b4
c4710n <c4710n@users.noreply.github.com>
2023-01-08 17:55:11 +0800

    init: mix new cozy_aliyun_open_api --module CozyAliyunOpenAPI

---
+++ lib/cozy_aliyun_open_api.ex
@@ -0,0 +1,18 @@
+defmodule CozyAliyunOpenAPI do
+  @moduledoc """
+  Documentation for `CozyAliyunOpenAPI`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CozyAliyunOpenAPI.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cozy_aliyun_open_api_test.exs
@@ -0,0 +1,8 @@
+defmodule CozyAliyunOpenAPITest do
+  use ExUnit.Case
+  doctest CozyAliyunOpenAPI
+
+  test "greets the world" do
+    assert CozyAliyunOpenAPI.hello() == :world
+  end
+end
