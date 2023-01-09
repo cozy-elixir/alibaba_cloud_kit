7d12b7095361c5b469d735b594821bcb3a361794
c4710n <c4710n@users.noreply.github.com>
2023-01-09 18:08:13 +0800

    chore: loose elixir version requirement

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule CozyAliyunOpenAPI.MixProject do
     [
       app: :cozy_aliyun_open_api,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: @description,
